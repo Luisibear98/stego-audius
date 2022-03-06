d22206d015e83407ba9885a165fd53960b453fc7
Luisibear98 <43785734+Luisibear98@users.noreply.github.com>
2022-03-06 22:38:52 +0100

    Update server.py

--- sockets/server.py
+++ sockets/server.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, unicode_literals
+from time import sleep
 from PyInquirer import prompt
 import socket
 import pickle
@@ -7,7 +8,12 @@ import numpy as np
 import threading
 from tqdm import tqdm
 from examples import custom_style_2
-
+import os, sys
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+import getpass
 
 modeSelector = {
     'type': 'list',
@@ -15,16 +21,29 @@ modeSelector = {
     'message': 'Select a mode',
     'choices': [
         'Fix text',
-        'Transfer',
-        'Help (?)',
+        'Streaming',
+        'Quit',
     ],
 }
-
 
 textInput = {
     'type': 'input',
     'name': 'text',
     'message': 'Type the texto to send',
+}
+
+confirmRerun = {
+    'type': 'confirm',
+    'message': 'Do you want to restart the program?',
+    'name': 'restart',
+    'default': True,
+}
+
+confirmDetection = {
+    'type': 'confirm',
+    'message': 'Are you sure?',
+    'name': 'detection',
+    'default': False,
 }
 
 flag = 0
@@ -56,21 +75,49 @@ class KeyboardThread(threading.Thread):
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+print_ascii()
+
 mode = prompt(modeSelector, style=custom_style_2)["modes"]
-
-if mode != "Help (?)":
-
-    embding_key = int(input("insert embedding key the higher the noiser: "))
+sure = False
 
 
-    communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+if mode != "Quit":
+    # Select embeding key
+    embding_key = input("From 0-10 how detectable do you want the secret message to be?: ")
+    try:
+        embeding_key = int(embding_key) + 35    # Embeding key is between 35 and 45 according to the papers results
+        if embeding_key > 45:
+            print("WARNING: Parameter over 10 is not recommendable")
+            sure = prompt(confirmDetection, style=custom_style_2)["detection"]
 
+            # If user wants to select a recommended value
+            while not sure:
+                embding_key = input("From 0-10 how detectable do you want the secret message to be?: ")
+                try:
+                    embeding_key = int(embding_key) + 35
+                    if embeding_key > 45:
+                        print("WARNING: Parameter over 10 is not recommendable")
+                        sure = prompt(confirmDetection, style=custom_style_2)["detection"]
+                    elif embeding_key < 35:
+                        print("ERROR: Parameter must be over 0")
+                        exit(-1)
+                except ValueError:
+                    print("ERROR: Input must be a number")
+                    exit(-1)
+        elif embeding_key < 35:
+            print("ERROR: Parameter must be over 0")
+            exit(-1)
+    except ValueError:
+        print("ERROR: Input must be a number")
+        exit(-1)
+
+    communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     communication_socket.connect((HOST, PORT+1))
     metadata = []
     if mode == "Fix text":
         metadata.append(1)
-    elif mode == "Transfer":
+    elif mode == "Streaming":
         metadata.append(2)
 
     if metadata:
@@ -85,22 +132,35 @@ if mode != "Help (?)":
 
 # Inserts text in audio and sends it in stream to he client
 if mode == "Fix text":
-    str = input("Introduce the text to send: ")
-    str = normalize(str)
-    insert_on_channel_1 = process_audio(str, embding_key)
+    text_str = bytes(input("Enter the text to send: "), "utf-8")
+    #text_str = normalize(text_str)
+    password = bytes(getpass.getpass(prompt="Enter password to encrypt text: "), "utf-8")
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=390000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+   
+    fernet = Fernet(key)
+    encrypted_text = fernet.encrypt(text_str)
+
+    insert_on_channel_1 = process_audio(encrypted_text, embding_key)
+
     print("Audio processed ...")
     to_send = serialize_channel(insert_on_channel_1)  # Serializing data
+    
+
     # Transforming information to bytes
+   
     bytes = np.array(to_send, np.int16).tobytes()
-    to_send = list(slice_chunks(bytes, 3000))  # Splitting in chunks of 12 kb
+    to_send = list(slice_chunks(bytes, 3000))  # Splitting in chunks of 3 kb
 
     # Opening socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
         s.bind((HOST, PORT))  # Binding socket
         s.listen()
         print("Waiting for a connection ...")
         metadata.append(1)
+        metadata.append(salt)
+        
         
         communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
         communication_socket.connect((HOST, PORT+1))
@@ -111,35 +171,37 @@ if mode == "Fix text":
             conn, addr = s.accept()
             with conn:
                 # Streaming bytes of audio with stego
+
                 for i in tqdm(range(len(to_send))):
                     conn.sendall(to_send[i])  # Ensuring all data arrives
-    print("Transminssion ended ...")
-
+                print("Transminssion ended ...")
+                print_bye()
 
 # Procesamiento en tiempo real
-elif mode == "Transfer":
-   #real time text sending
+elif mode == "Streaming":
+    #real time text sending
     audio = read_wav()
     channel_1 = audio[0]    
     insert = []
    
-    channel_1_splited = split_channel(channel_1)
+    channel_1_splited = split_channel(channel_1,3)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         send_info = chr(0x02)
-
+        
         s.bind((HOST, PORT))
         s.listen()
-        print("listening")
+        print("Listening...")
         position = 0
         sent = 0
         processing = 0
-        print("sending flag")
-        metadata.append(1)
+        print("Sending flag...")
+        metadata.append(2)
         communication_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
         communication_socket.connect((HOST, PORT+1))
         communication_socket.sendall(pickle.dumps(metadata))
         communication_socket.close()
+        flag = 0
         print("Now you can insert data on the fly")
         while 1:
             conn, addr = s.accept()
@@ -148,13 +210,14 @@ elif mode == "Transfer":
                 #envío del audio con texto
                 kthread = KeyboardThread(my_callback)
                 for position in range(len(channel_1_splited)):
-                 
-                    multiple = sent % 8
-                    if flag and multiple == 0 :
+             
+                    if flag and sent % 8 == 0:
                         if not processing:
                             start = [1]
                             send_package = pickle.dumps(start)
+                            print("ey")
                             conn.sendall(send_package)
+                            sleep(1)
                             processing = 1
             
                         bina = binarize(send_info)
@@ -166,38 +229,32 @@ elif mode == "Transfer":
                         
                         for i in range(8):
                             insert.append(1)
-                       
+                        #print(insert)
                         while bit_sent < len(insert):
                           
-                            package,inserted= insert_on_real_time(insert[bit_sent],channel_1_splited[position],key=50)
+                            package,inserted= insert_on_real_time(insert[bit_sent],channel_1_splited[position],embding_key)
+                            #print(channel_1_splited[position])
+                            
                             if inserted:
                                 bit_sent += 1
                             send_package = pickle.dumps(package)
-                           
+                          
+                          
                             conn.sendall(send_package)
                             position += 1
+                        print("end")
                         flag = 0
                         send_info = chr(0x02)
                         processing = 0
                       
                     else:
-                      
                         package = channel_1_splited[position]
                         send_package = pickle.dumps(package)
                         conn.sendall(send_package)
                         sent += 1
-                        
-                      
-                  
-
-elif mode == "Help (?)":
-    print("\
-        This tool lets you send hidden and encrypted messages via network sockets\
-        inside any song, the available options are:\n\
-        'Fix text: Ask you for a text and inserts it into the song, the client plays the music in streaming',\
-        'Transfer: You can transfer a song to the client and insert any song during the process'\
-    ")
-    #Add Y/n
-    #os.execl(sys.executable, sys.executable, *sys.argv)
 
 
+
+else:
+    print_bye()
+    exit()
